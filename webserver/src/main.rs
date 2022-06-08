47f2c7de2d88f1ad4d7fdea2a0df37744fee548e
Asura <todaymofish@gmail.com>
2022-06-08 02:51:20 -0700

    feat: Add threadpool

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,6 +1,9 @@
 use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread::{self, Thread};
+use std::time::Duration;
+use webserver::ThreadPool;
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
@@ -8,7 +11,12 @@ fn handle_connection(mut stream: TcpStream) {
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
@@ -23,10 +31,20 @@ fn handle_connection(mut stream: TcpStream) {
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established!");
 
-        handle_connection(stream);
+        // handle_connection(stream);
+
+        pool.execute(||{
+            handle_connection(stream)
+        })
+
+        // thread::spawn(|| {
+        //     handle_connection(stream);
+        // });
     }
 }
