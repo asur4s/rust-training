b3f39c3bc6e955382a56e42cee28849474ce2ada
Asura <todaymofish@gmail.com>
2022-06-10 02:45:17 -0700

    fix: search_case_insensitive

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -9,15 +9,21 @@ pub struct Config {
 }
 
 impl Config {
+    // 函数通常使用 Result 作为返回类型，如果成功就返回 OK，如果失败就返回 Err。
+    // Result 左边是 OK 的类型，右边是 Err 的类型。不能改变顺序
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
 
+        // 由于 args 是借用，所以必须使用 clone 获取所有权。
         let query = args[1].clone();
         let filename = args[2].clone();
 
+        // is_err 可以判断是否出错。如果存在该变量（忽略大小写），就为 False，如果不存在就为 True。
+        // case sensitive：区分大小写
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        println!("{:?}", case_sensitive);
 
         Ok(Config {
             query,
@@ -27,10 +33,16 @@ impl Config {
     }
 }
 
+// 这里的 Error 采用特征对象（Box<dyn Error>）
+// todo: 为什么不能使用 &dyn Error
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // ? 是一个宏，主要用于传播错误。
+    // ? 会调用 From 特质的 from 方法，自动进行类型提升
+    // ? 还可以自动转换 Option 返回类型。
     let contents = fs::read_to_string(config.filename)?;
 
     let results = if config.case_sensitive {
+        // 传入引用
         search(&config.query, &contents)
     } else {
         search_case_insensitive(&config.query, &contents)
@@ -42,10 +54,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+// 返回引用 需要标注生命周期。
+// 字符串的内容来源于 content，所以生命周期和 content 相同。
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
     for line in contents.lines() {
+        // 判断字符串中是否包含查询内容
         if line.contains(query) {
             results.push(line);
         }
@@ -57,7 +72,8 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
     let mut results = Vec::new();
 
     for line in contents.lines() {
-        if line.to_lowercase().contains(query) {
+        // 改变内容的大小写再查询。
+        if line.to_lowercase().contains(&query.clone().to_lowercase()) {
             results.push(line);
         }
     }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,7 +2,12 @@ use minigrep::Config;
 use std::{env, process};
 
 fn main() {
+    // env::args 会返回一个迭代器，迭代器会将参数以 OSString 类型返回，
+    // 使用 collect 可以将迭代器转换为数组。
     let args: Vec<String> = env::args().collect();
+
+    // 如果返回 Options 类型，就执行 unwrap 展开，否则展开后执行闭包的内容。
+    // 闭包接收 Err 中的字符串，然后打印，并退出。
     let config = Config::new(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
