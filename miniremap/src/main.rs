a000faa7be2a014022045e8f8c7bef1e62fe9ad0
Asura <todaymofish@gmail.com>
2022-06-11 08:43:35 -0700

    feat(output.rs): Add output device

--- miniremap/src/main.rs
+++ miniremap/src/main.rs
@@ -1,9 +1,33 @@
 use std::error::Error;
 use std::path::PathBuf;
 
+use evdev::{Device, EventType};
+
 mod config;
 mod input;
+mod output;
 mod select;
+
+fn event_loop(input_device: &mut Device) -> Result<(), Box<dyn Error>> {
+    let mut output_device = output::build_device(input_device).unwrap();
+
+    // 捕获事件
+    for _ in 0..5 {
+        if !select::is_readable(input_device) {
+            continue;
+        }
+        for event in input_device.fetch_events().unwrap() {
+            if event.event_type() == EventType::KEY {
+                println!("event: {:?}", event);
+            }
+            // 虚拟设备 emit 有什么作用？
+            // 将事件提交给操作系统。
+            output_device.emit(&[event]).unwrap();
+        }
+    }
+
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // env_logger::init();
@@ -12,11 +36,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut device = input::select_device();
     device.grab()?;
-
-    if select::is_readable(&mut device) {
-        println!("event!");
-    }
-
+    event_loop(&mut device)?;
     device.ungrab()?;
 
     Ok(())
---
+++ miniremap/src/output.rs
@@ -0,0 +1,16 @@
+use evdev::uinput::{VirtualDevice, VirtualDeviceBuilder};
+use evdev::{AttributeSet, Key, Device};
+use std::error::Error;
+
+pub fn build_device(base_device: &Device) -> Result<VirtualDevice, Box<dyn Error>> {
+    // let mut keys = AttributeSet::<Key>::new();
+    // keys.insert(Key::KEY_A);
+
+    let device = VirtualDeviceBuilder::new()?
+        .name("minixremap")
+        .with_keys(base_device.supported_keys().unwrap())?
+        .build()
+        .unwrap();
+
+    Ok(device)
+}
