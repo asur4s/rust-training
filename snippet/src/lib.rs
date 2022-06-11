b510d437d62b091fc44a82074526be03ac251e2d
Asura <todaymofish@gmail.com>
2022-06-11 01:51:15 -0700

    fix(test_deref): Add Box deref and function deref

--- snippet/src/lib.rs
+++ snippet/src/lib.rs
@@ -7,7 +7,6 @@ pub fn test_box() {
     println!("b = {:?}", b);
 }
 
-
 pub fn test_box_list() {
     enum List {
         Cons(i32, Box<List>),
@@ -15,20 +14,53 @@ pub fn test_box_list() {
     };
 
     use List::*;
-    let list = Cons(
-        1,
-        Box::new(
-            Cons(2, Box::new(Nil))
-        )
-    );
-
+    let list = Cons(1, Box::new(Cons(2, Box::new(Nil))));
 }
 
-
-pub fn test_deref(){
-    let x= 5;
+pub fn test_deref() {
+    let x = 5;
     let y = &x;
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    struct MyBox<T>(T);
+
+    impl<T> MyBox<T> {
+        fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    }
+
+    use std::ops::Deref;
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+
+        fn deref(&self) -> &T {
+            &self.0
+        }
+    }
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    fn function_deref(name: &str) {
+        println!("[*] function_deref: Hello, {:?}", name);
+    }
+    let m = MyBox::new(String::from("Rust"));
+    // &MyBox(String) -> &String -> &str：函数参数会自动解引用。
+    // & 表示借用。
+    function_deref(&m);
+    // 手动解引用
+    function_deref(&(*m)[..])
+
 }
